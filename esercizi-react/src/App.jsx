aed87506afae6e89a906b59cd33b004c8ca75d3d
Martina Costantino <martinacostantino@yahoo.it>
2025-03-20 23:16:49 +0100

    map

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Colors from './Colors'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const colorList = [
+    { id: 1, name: "Rosso" },
+    { id: 2, name: "Blu" },
+    { id: 3, name: "Verde" },
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Colors colors={colorList}></Colors>
+
     </>
   )
 }
---
+++ esercizi-react/src/Color.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Color({color}){
+  return <li>{color.name}</li>
+}
